052d7c40d20b6c0162cf2287210f8c31394becce
Pietro Rampazzo <pietro@rampazzo.co>
2020-03-22 12:19:04 +0100

    Custom icon (not completed)

--- assets/map.js
+++ assets/map.js
@@ -2,18 +2,26 @@ var map = L.map("mapid").setView([45.40797, 11.88586], 13);
 map.locate({ setView: false, maxZoom: 16 });
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 var Marker,
-    Circle = {};
+  Circle = {};
+
+// Custom Icon
+
+// var houseIcon = L.icon({
+//   iconUrl: "assets/icons/house.png",
+//   iconSize: [56, 56],
+// });
 
 function onLocationFound(e) {
-    addPoint(e, "Questa è l'area concessa per la posizione rilevata")
+  addPoint(e, "Questa è l'area concessa per la posizione rilevata");
 }
 
 // onLocationError
+
 // function onLocationError(e) {
 //     if (e.code === 1) {
 //         alert("Puoi cliccare sulla mappa per visualizzare l'area concessa");
@@ -26,23 +34,23 @@ function onLocationFound(e) {
 // map.on('locationerror', onLocationError);
 
 function addPoint(e, text) {
-    lat = e.latlng.lat;
-    lon = e.latlng.lng;
+  lat = e.latlng.lat;
+  lon = e.latlng.lng;
 
-    // Clear existing
-    if (Marker !== undefined) {
-        map.removeLayer(Marker);
-        map.removeLayer(Circle);
-    }
+  // Clear existing
+  if (Marker !== undefined) {
+    map.removeLayer(Marker);
+    map.removeLayer(Circle);
+  }
 
-    // Add a marker+circle to show where you clicked
-    Marker = L.marker([lat, lon]).addTo(map);
-    if (text !== undefined) {
-        Marker.bindPopup(text).openPopup();
-    }
-    Circle = L.circle([lat, lon], 200).addTo(map);
-    map.setView(e.latlng, 16);
+  // Add a marker+circle to show where you clicked
+  Marker = L.marker([lat, lon]).addTo(map);
+  if (text !== undefined) {
+    Marker.bindPopup(text).openPopup();
+  }
+  Circle = L.circle([lat, lon], 200).addTo(map);
+  map.setView(e.latlng, 16);
 }
 
-map.on('locationfound', onLocationFound);
+map.on("locationfound", onLocationFound);
 map.on("click", addPoint);

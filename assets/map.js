307ae9b6f50da830947a2d0886bd9c9811e76d7a
Pietro Rampazzo <pietro@rampazzo.co>
2020-04-17 10:21:17 +0200

    change radius feature

--- assets/map.js
+++ assets/map.js
@@ -3,7 +3,7 @@ map.locate({ setView: false, maxZoom: 16 });
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
 L.easyButton('<i class="fas fa-map-marker"></i>', (btn, map) => {
@@ -28,11 +28,15 @@ L.easyButton('<i class="fas fa-map-marker"></i>', (btn, map) => {
 //   })
 //   .addTo(map);
 
-var Marker,
+let Marker,
   Circle = {};
 
+let latlng = {};
+let radius = 200;
+
 function onLocationFound(e) {
-  addPoint(e, "Questa è l'area concessa per la posizione rilevata");
+  latlng = e.latlng;
+  addPoint("Questa è l'area concessa per la posizione rilevata.");
 }
 
 // onLocationError
@@ -48,10 +52,7 @@ function onLocationFound(e) {
 
 // map.on('locationerror', onLocationError);
 
-function addPoint(e, text) {
-  lat = e.latlng.lat;
-  lon = e.latlng.lng;
-
+function addPoint(text) {
   // Clear existing
   if (Marker !== undefined) {
     map.removeLayer(Marker);
@@ -59,13 +60,50 @@ function addPoint(e, text) {
   }
 
   // Add a marker+circle to show where you clicked
-  Marker = L.marker([lat, lon]).addTo(map);
+  Marker = L.marker([latlng.lat, latlng.lng]).addTo(map);
   if (text !== undefined) {
-    Marker.bindPopup(text).openPopup();
+    Marker.bindPopup(
+      text + "<br>Il raggio è di " + radius + " metri."
+    ).openPopup();
+  } else {
+    Marker.bindPopup("Il raggio è di " + radius + " metri.").openPopup();
   }
-  Circle = L.circle([lat, lon], 200).addTo(map);
-  map.setView(e.latlng, 15);
+  Circle = L.circle([latlng.lat, latlng.lng], parseInt(radius)).addTo(map);
+  map.setView(latlng, 15);
 }
 
 map.on("locationfound", onLocationFound);
-map.on("click", addPoint);
+map.on("click", (e) => {
+  latlng = e.latlng;
+  addPoint();
+});
+
+// Tools
+
+const collapse = document.querySelector("#collapse");
+const tools = document.querySelector("#tools");
+const inputRadius = document.querySelector("#radius");
+const bubble = document.querySelector(".bubble");
+
+inputRadius.addEventListener("input", () => {
+  const val = inputRadius.value;
+  const min = inputRadius.min ? inputRadius.min : 0;
+  const max = inputRadius.max ? inputRadius.max : 100;
+  const newVal = Number(((val - min) * 100) / (max - min));
+  bubble.innerHTML = val;
+  bubble.style.left = `calc(${newVal}% + (${8 - newVal * 0.15}px))`;
+
+  radius = val;
+  addPoint();
+});
+
+collapse.addEventListener("click", () => {
+  console.log(tools.className);
+  if (tools.style.height === "340px") {
+    tools.style.height = "5px";
+    collapse.className = "fas fa-chevron-circle-down";
+  } else {
+    tools.style.height = "340px";
+    collapse.className = "fas fa-times-circle";
+  }
+});
